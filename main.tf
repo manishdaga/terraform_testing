7fcbe1caba231efcbf533ce4c07b9d6c5f6a3168
Manish Daga <manishdagamanish@gmail.com>
2023-02-20 19:13:40 +0530

    added glue job code

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 variable "file_change" {
-  type = string
+  type    = list(string)
+  default = []
 }
-resource "aws_ssm_parameter" "foo" {
-  name  =  var.file_change
-  type  = "String"
-  value = "barr"
+resource "null_resource" "run_glue_job" {
+  provisioner "local-exec" {
+    command = "aws glue start-job-run --job-name run_job_from_terraform --arguments '--config_file=${var.file_change}'"
+  }
 }

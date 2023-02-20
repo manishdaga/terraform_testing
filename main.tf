7f6dde6729995ed763b1565c96f75506db91f4c6
Manish Daga <manishdagamanish@gmail.com>
2023-02-20 19:36:10 +0530

    change in config file

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ variable "file_change" {
 }
 resource "null_resource" "run_glue_job" {
   provisioner "local-exec" {
-    command = "aws glue start-job-run --job-name example --arguments '--config=${var.file_change}'"
+    command = "aws glue start-job-run --job-name example --arguments 'config_file=${var.file_change}'"
   }
 }

79e717f4d432ba7539a571023f2c5e61e5daf206
Harish Daga <harishdaga@Harishs-Mac-mini.local>
2023-08-22 18:30:31 +0530

    added dms config file

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ variable "file_change" {
 }
 resource "null_resource" "run_glue_job" {
   provisioner "local-exec" {
-    command = "aws glue start-job-run --job-name AddTablesToDMSNEW --arguments 'config_file_path=${var.file_change}'"
+    command = "aws glue start-job-run --job-name add_table_toDMS_new_code --arguments 'config_file_path=${var.file_change}'"
   }
 }
